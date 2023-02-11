d662fd15bff9e98956631078023bd6ada5d2c33d
klaudiabilska <klaudiabilska1@gmail.com>
2023-02-11 18:49:00 +0100

    edit

--- js.js
+++ js.js
@@ -1,14 +1,14 @@
 // Date object
 
 let currentDate = new Date();
-console.log(currentDate)
+console.log(currentDate);
 
 let hour = currentDate.getHours();
 let min = currentDate.getMinutes();
-console.log(hour + ":" + min)
+console.log(hour + ":" + min);
 
 
-console.log(currentDate.toLocaleString('deflaut', {weekday: 'long'}))
+console.log(currentDate.toLocaleString('deflaut', {weekday: 'long'}));
 
 /// Math object
 
@@ -16,12 +16,12 @@ const num1 = Math.random().toFixed(1) * 10
 const num2 = Math.random().toFixed(1) * 10 
 const num3 = Math.random().toFixed(1) * 10 
 
-console.log(num1)
-console.log(num2)
-console.log(num3)
+console.log(num1);
+console.log(num2);
+console.log(num3);
 
 const max = Math.max(num1, num2, num3)
-console.log(max)
+console.log(max);
 
 // Comparison Operators
 
@@ -30,12 +30,22 @@ const b = 15
 
 console.log(
     a > b
-)
+);
 
 console.log(
     a == "10"
-)
+);
 
 console.log(
     a === "10"
-)
+);
+
+// Nullish Coalescing Operator
+
+let course;
+console.log(course ?? "Please select a course")
+
+let courseProfress = 0;
+console.log(courseProfress ?? "Start the cours")
+
+// Control flow - By default our JavaScript code is executed from top to bottom, line by line however we can change this with control flow. Control flow allows our program to make decisions about what code is executed and when
